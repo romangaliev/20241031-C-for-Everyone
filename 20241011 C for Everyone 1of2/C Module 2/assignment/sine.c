a775062681e0955a9ad21b47123553ac1d3a82e2
Roman Galiev <2roman.galiev@gmail.com>
2024-11-02 21:43:00 +0200

    Peer-graded Assignment: Write a program that prints the sine function -  DONE

---
+++ 20241011 C for Everyone 1of2/C Module 2/assignment/sine.c
@@ -0,0 +1,33 @@
+//a program that prints the sine function
+//for an input x between (0, 1)
+
+#include <stdio.h> // this is to scan and print
+
+#include <math.h> // this is to have a sine function and M_PI
+
+/* the below ifndef and endif are to define M_PI
+if it is not defined in <math.h> */
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+int main(void)
+{
+	float x;
+	float result;
+
+	printf("Hi!\n"); 
+	printf("This program prints the sine function\n"); 
+	printf("for an input x between (0, 1)\n\n" );
+	printf("Please provide the input x between (0, 1)\n"); 
+
+	scanf("%f", &x); // scaning into memory the provided input
+
+	printf("You provided the input x =  %f \n\n", x); 
+
+	result = sin(x*M_PI); // M_PI is to convert to radians
+	
+	printf("The sine of x is equal to %f\n", result);
+
+	return 0;
+}
